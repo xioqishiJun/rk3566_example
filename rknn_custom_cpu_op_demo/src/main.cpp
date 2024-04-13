c8467d998cd45d5f3a65aa81ad12bc21605c09a5
xioqishi <xioqishi@126.com>
2024-04-14 01:19:24 +0800

    working,but not correct

--- rknn_custom_cpu_op_demo/src/main.cpp
+++ rknn_custom_cpu_op_demo/src/main.cpp
@@ -5,6 +5,10 @@
 #include <string>
 #include <cstring>
 #include "rknn_api.h"
+#include <cstdio>
+#include <cstdlib>
+#include <sys/time.h>
+#include <float.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 
@@ -13,6 +17,14 @@
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 
 #include <stb/stb_image_resize.h>
+
+static inline int64_t getCurrentTimeUs()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
 static unsigned char *load_image(const char *image_path, rknn_tensor_attr *input_attr)
 {
     int req_height = 0;
@@ -41,7 +53,9 @@ static unsigned char *load_image(const char *image_path, rknn_tensor_attr *input
     int width = 0;
     int channel = 0;
 
+    // 使用stb库加载图片
     unsigned char *image_data = stbi_load(image_path, &width, &height, &channel, req_channel);
+    printf("image_data size: %d\n", sizeof(image_data));
     if (image_data == NULL)
     {
         printf("load image failed!\n");
@@ -57,6 +71,7 @@ static unsigned char *load_image(const char *image_path, rknn_tensor_attr *input
             STBI_FREE(image_data);
             return NULL;
         }
+
         if (stbir_resize_uint8(image_data, width, height, 0, image_resized, req_width, req_height, 0, channel) != 1)
         {
             printf("resize image failed!\n");
@@ -96,6 +111,7 @@ static unsigned char *load_model(const char *filename, int *model_size)
     }
     return model;
 }
+
 // 打印张量信息
 static void dump_tensor_attr(rknn_tensor_attr *attr)
 {
@@ -106,10 +122,42 @@ static void dump_tensor_attr(rknn_tensor_attr *attr)
             get_qnt_type_string(attr->qnt_type), attr->zp, attr->scale);
 }
 
+static int rknn_GetTopN(float *pfProb, float *pfMaxProb, uint32_t *pMaxClass, uint32_t outputCount, uint32_t topNum)
+{
+    uint32_t i, j;
+    uint32_t top_count = outputCount > topNum ? topNum : outputCount;
+
+    for (i = 0; i < topNum; ++i)
+    {
+        pfMaxProb[i] = -FLT_MAX;
+        pMaxClass[i] = -1;
+    }
+
+    for (j = 0; j < top_count; j++)
+    {
+        for (i = 0; i < outputCount; i++)
+        {
+            if ((i == *(pMaxClass + 0)) || (i == *(pMaxClass + 1)) || (i == *(pMaxClass + 2)) || (i == *(pMaxClass + 3)) ||
+                (i == *(pMaxClass + 4)))
+            {
+                continue;
+            }
+
+            if (pfProb[i] > *(pfMaxProb + j))
+            {
+                *(pfMaxProb + j) = pfProb[i];
+                *(pMaxClass + j) = i;
+            }
+        }
+    }
+
+    return 1;
+}
+
 int main()
 {
-    char *model_path = "../model/az_handwriting3566.rknn";
-    char *img_path = "../model/test_image.jpg";
+    char *model_path = "../model/az_handwriting3588.rknn";
+    char *img_path = "../model/a.png";
 
     rknn_context ctx = 0;
     int ret = 0;
@@ -188,17 +236,180 @@ int main()
     }
     printf("custom string: %s\n", custom_string.string);
 
+    // step5 加载图片
     unsigned char *input_data = NULL;
     rknn_tensor_type input_type = RKNN_TENSOR_UINT8;
     rknn_tensor_format input_layout = RKNN_TENSOR_NHWC;
-
-
-    // step5 加载图片
     input_data = load_image(img_path, &input_attrs[0]);
 
     if (!input_data)
     {
         return -1;
     }
+
+
+
+
+    // step 6 创建输入输出张量内存
+    // 输入张量内存
+    rknn_tensor_mem *input_mems[1];
+    // default input type is int8 (normalize and quantize need compute in outside)
+    // if set uint8, will fuse normalize and quantize to npu
+    input_attrs[0].type = input_type;
+    // default fmt is NHWC, npu only support NHWC in zero copy mode
+    input_attrs[0].fmt = input_layout;
+
+    input_mems[0] = rknn_create_mem(ctx, input_attrs[0].size_with_stride);
+
+    // Copy input data to input tensor memory
+    int width = input_attrs[0].dims[2];
+    int stride = input_attrs[0].w_stride;
+
+    if (width == stride)
+    {
+        memcpy(input_mems[0]->virt_addr, input_data, width * input_attrs[0].dims[1] * input_attrs[0].dims[3]);
+    }
+    else
+    {
+        int height = input_attrs[0].dims[1];
+        int channel = input_attrs[0].dims[3];
+        // copy from src to dst with stride
+        uint8_t *src_ptr = input_data;
+        uint8_t *dst_ptr = (uint8_t *) input_mems[0]->virt_addr;
+        // width-channel elements
+        int src_wc_elems = width * channel;
+        int dst_wc_elems = stride * channel;
+        for (int h = 0; h < height; ++h)
+        {
+            memcpy(dst_ptr, src_ptr, src_wc_elems);
+            src_ptr += src_wc_elems;
+            dst_ptr += dst_wc_elems;
+        }
+    }
+
+    // 输出张量内存
+    rknn_tensor_mem *output_mems[io_num.n_output];
+    for (uint32_t i = 0; i < io_num.n_output; ++i)
+    {
+        // default output type is depend on model, this require float32 to compute top5
+        // allocate float32 output tensor
+        int output_size = output_attrs[i].n_elems * sizeof(float);
+        output_mems[i] = rknn_create_mem(ctx, output_size);
+    }
+
+    // Set input tensor memory
+    ret = rknn_set_io_mem(ctx, input_mems[0], &input_attrs[0]);
+    if (ret < 0)
+    {
+        printf("rknn_set_io_mem fail! ret=%d\n", ret);
+        return -1;
+    }
+
+    // Set output tensor memory
+    for (uint32_t i = 0; i < io_num.n_output; ++i)
+    {
+        // default output type is depend on model, this require float32 to compute top5
+        output_attrs[i].type = RKNN_TENSOR_FLOAT32;
+        // set output memory and attribute
+        ret = rknn_set_io_mem(ctx, output_mems[i], &output_attrs[i]);
+        if (ret < 0)
+        {
+            printf("rknn_set_io_mem fail! ret=%d\n", ret);
+            return -1;
+        }
+    }
+
+    int loop_count = 1;
+    // Run
+    printf("rknn_run\n");
+    ret = rknn_run(ctx, nullptr);
+    if (ret < 0)
+    {
+        printf("rknn_run fail! ret=%d\n", ret);
+        return -1;
+    }
+
+    // Get Output
+    rknn_output outputs[1];
+    memset(outputs, 0, sizeof(outputs));
+    outputs[0].want_float = 1;
+    ret = rknn_outputs_get(ctx, 1, outputs, NULL);
+    if (ret < 0)
+    {
+        printf("rknn_outputs_get fail! ret=%d\n", ret);
+        return -1;
+    }
+
+
+    // Post Process
+    for (int i = 0; i < io_num.n_output; i++)
+    {
+        uint32_t MaxClass[5];
+        float fMaxProb[5];
+        float *buffer = (float *) outputs[i].buf;
+        uint32_t sz = outputs[i].size / 4;
+
+        rknn_GetTopN(buffer, fMaxProb, MaxClass, sz, 5);
+
+        printf(" --- Top5 ---\n");
+        for (int i = 0; i < 5; i++)
+        {
+            printf("%3d: %8.6f\n", MaxClass[i], fMaxProb[i]);
+        }
+    }
+//    printf("Begin perf ...\n");
+//    for (int i = 0; i < loop_count; ++i)
+//    {
+//        int64_t start_us = getCurrentTimeUs();
+//        ret = rknn_run(ctx, NULL);
+//        int64_t elapse_us = getCurrentTimeUs() - start_us;
+//        if (ret < 0)
+//        {
+//            printf("rknn run error %d\n", ret);
+//            return -1;
+//        }
+//        printf("%4d: Elapse Time = %.2fms, FPS = %.2f\n", i, elapse_us / 1000.f, 1000.f * 1000.f / elapse_us);
+//    }
+
+
+// Get top 5
+//    uint32_t topNum = 5;
+//    for (uint32_t i = 0; i < io_num.n_output; i++)
+//    {
+//        uint32_t MaxClass[topNum];
+//        float fMaxProb[topNum];
+//        float *buffer = (float *) output_mems[i]->virt_addr;
+//        uint32_t sz = output_attrs[i].n_elems;
+//        int top_count = sz > topNum ? topNum : sz;
+//
+//        rknn_GetTopN(buffer, fMaxProb, MaxClass, sz, topNum);
+//
+//        printf("---- Top%d ----\n", top_count);
+//        for (int j = 0; j < top_count; j++)
+//        {
+//            printf("%8.6f - %d\n", fMaxProb[j], MaxClass[j]);
+//        }
+//    }
+
+    // Destroy rknn memory
+    rknn_destroy_mem(ctx, input_mems[0]);
+    for (uint32_t i = 0; i < io_num.n_output; ++i)
+    {
+        rknn_destroy_mem(ctx, output_mems[i]);
+    }
+
+    // destroy
+    rknn_destroy(ctx);
+
+    if (input_data != nullptr)
+    {
+        free(input_data);
+    }
+
+    if (model != nullptr)
+    {
+        free(model);
+    }
+
     return 0;
 }
